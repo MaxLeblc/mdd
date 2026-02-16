6bdf43634be79ea1110028111438d728be842f56
Maxime Leblanc <maxime.leb@gmail.com>
2026-02-16 12:11:35 +0100

    fix: display posts list on front with BDD infos

--- backend/src/main/java/com/openclassrooms/mdd/repository/PostRepository.java
+++ backend/src/main/java/com/openclassrooms/mdd/repository/PostRepository.java
@@ -2,6 +2,16 @@ package com.openclassrooms.mdd.repository;
 
 import com.openclassrooms.mdd.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.domain.Sort;
+import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @EntityGraph(attributePaths = {"author", "topic"})
+    List<Post> findAll(Sort sort);
+
+    @EntityGraph(attributePaths = {"author", "topic"})
+    Optional<Post> findById(Long id);
 }
