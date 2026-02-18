23cc71d1d1de7b3b7d73b797c7d1b0cc1c768942
Maxime Leblanc <maxime.leb@gmail.com>
2026-02-18 18:30:09 +0100

    wip: add register component and modify AuthController to directy login after register

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { LoginRequest, JwtResponse } from '../models/auth.model';
+import { LoginRequest, RegisterRequest, JwtResponse } from '../models/auth.model';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -14,6 +14,10 @@ export class AuthService {
 
   login(credentials: LoginRequest): Observable<JwtResponse> {
     return this.http.post<JwtResponse>(`${this.apiUrl}/login`, credentials);
+  }
+
+  register(data: RegisterRequest): Observable<JwtResponse> {
+    return this.http.post<JwtResponse>(`${this.apiUrl}/register`, data);
   }
 
   saveToken(token: string): void {
